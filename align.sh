3ab2b80ac1d699ced9a8ef7461051c149c59b971
David Hunt <davhunt@indiana.edu>
2019-05-17 13:07:48 -0400

    prf

--- align.sh
+++ align.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd prf
 for i in 'polarAngle' 'eccentricity' 'exponent' 'rfWidth' 'r2' 'gain' 'meanvol';
 do gunzip -k ./"$i".nii.gz && nifti_tool -mod_hdr -prefix ./"$i"_aligned.nii -mod_field srow_x '-1.6 0 0 90' -mod_field srow_y '0 1.6 0 -126' -mod_field srow_z '0 0 1.60 -72' -infiles ./"$i".nii
 done
+cd ..
 
 #gunzip -k ./results.nii.gz && nifti_tool -mod_hdr -prefix ./results_aligned.nii -mod_field srow_x '-1.6 0 0 90' -mod_field srow_y '0 1.6 0 -126' -mod_field srow_z '0 0 1.60 -72' -infiles ./results.nii
