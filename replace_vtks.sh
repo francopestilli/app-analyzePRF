cef1219a83e5a2e085958ec5f6943423f11c1a04
David Hunt <davhunt@indiana.edu>
2020-03-31 19:11:30 -0400

    make vtks line up

--- replace_vtks.sh
+++ replace_vtks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i = pial white inflated sphere
+for i in pial white inflated sphere
 do
-for hemi = lh rh
+for hemi in lh rh
 do
 rm prf/surfaces/${hemi}.${i}.vtk
 cp /N/u/davhunt/Carbonate/Downloads/Bradley_prf/surfaces/${hemi}.${i}.gii prf/surfaces/${hemi}.${i}.gii
